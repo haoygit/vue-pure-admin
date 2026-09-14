a1cf60d3a192d161938190fe464d437231f8152c
yanghao <18200782152@163.com>
2026-09-14 11:51:33 +0800

    feat: :sparkles: 修改列表单位

--- src/views/welcome/components/table/columns.tsx
+++ src/views/welcome/components/table/columns.tsx
@@ -17,7 +17,7 @@ export function useColumns() {
     },
     {
       sortable: true,
-      label: "需求人数",
+      label: "需求人数（个）",
       prop: "requiredNumber",
       filterMultiple: false,
       filterClassName: "pure-table-filter",
